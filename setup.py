e34b7feb454768498aa009986861eb7b7fae6de6
Ingrid Vanessa Mora Sanchez <i.moras@uniandes.edu.co>
2025-02-02 18:46:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,16 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='malaria-clones',
-    version='1.0.0',
-    description='A pipeline for peak detection and analysis in malaria clones data',
-    author='Ingrid Vanessa Mora Sanchez',
-    author_email='i.moras@uniandese.edu.com',
-    url='https://github.com/ingridvmoras/malaria-clones',  # Replace with your project's URL
-    packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'pandas',
-        'seaborn',
-        'matplotlib',
-        
-    ],
-    entry_points={
-        'console_scripts': [
-            'run-pipeline=main:main',  
-        ],
-    },
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
-)
+install_requires=[
+        'numpy>=1.18.0',
+        'pandas>=1.0.0',
+        'matplotlib>=3.1.0',
+        'seaborn>=0.10.0',
+        'scipy>=1.4.0',
+        'statannot>=0.2.3',
+        'findpeaks>=1.0.5',
+        'pytest>=5.0.0',
+        'scikit-learn>=0.22.0'
+    ]
+packages=find_packages(install_requires=install_requires),
+
+
